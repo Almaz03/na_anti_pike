d6a1818b8b83c2628e474aaf2d1ae6eee9d26b28
Almaz0304 <akbaralyev.almaz@gmail.com>
2023-03-21 23:39:15 +0300

    v 0.1a added template

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-print("Hello Egor!")
+import requests
+from bs4 import BeautifulSoup
+import telegram
 
-govno = print
-govno("АЛМАЗ ЛОХ")
-print("1")
+# Получаем HTML-код страницы с расписанием
+group_url = 'https://mai.ru/education/studies/schedule/index.php?group=%D0%9C7%D0%9E-108%D0%A1-22'
+response = requests.get(group_url)
+html = response.content
+
+# Парсим HTML-код страницы
+soup = BeautifulSoup(html, 'html.parser')
+table = soup.find('table', {'class': 'schedule_table'})
+
+# Формируем сообщение с расписанием для отправки в Telegram-бот
+message = ''
+for row in table.find_all('tr')[1:]:
+    columns = row.find_all('td')
+    lesson = columns[1].text.strip()
+    time = columns[0].text.strip()
+    message += f'{time} - {lesson}\n'
+
+# Отправляем сообщение в Telegram-бот
+bot_token = 'YOUR_BOT_TOKEN'
+chat_id = 'YOUR_CHAT_ID'
+bot = telegram.Bot(token=bot_token)
+bot.send_message(chat_id=chat_id, text=message)
