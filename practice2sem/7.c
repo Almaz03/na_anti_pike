1ad0ee8adb3cd73ed600b511bca6f96e4c39ee9b
Almaz Akbaralyev <53372807+Almaz03@users.noreply.github.com>
2023-07-19 15:21:45 +0300

    Add files via upload

--- practice2sem/7.c
+++ practice2sem/7.c
@@ -3,11 +3,17 @@
 
 int main() {
 
-
     double k;
-    k = iauS00b(2450123.7, 0.);
 
+
+
+    k = iauS00b(2460133.02388, 2460129.02388);
+    printf_s("%lf\n", k);
+
+
+    k = iauS00b(2450123.7, 0.);
     printf("%lf\n", k);
+
 
     k = iauS00b(2450123.5, 0.2);
     printf("%lf\n", k);
